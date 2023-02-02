c2d24e056bd3bec3feb1cf7b86fc36cab8d35d9e
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-02 11:58:49 -0600

    fixed that damn button

--- js/movies.js
+++ js/movies.js
@@ -1,26 +1,27 @@
 (function () {
 
-    let movies = $("#movies")
 
     $("#addBtn").click(function () {
-        const newMovie = {
-            title : $("#titleInput").val(),
-            genre : $("#genreInput").val(),
-            rating : $("#ratingInput").val(),
-            description : $("#descriptionInput").val()
-        }
-        const url = 'https://tender-soft-bayberry.glitch.me/movies/';
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(newMovie),
-        };
-        fetch(url, options)
-            .then(response => console.log(response)) /* review was created successfully */
-            .catch(error => console.error(error)); /* handle errors */
+            console.log('hello')
+            const newMovie = {
+                title: $("#titleInput").val(),
+                rating: $("#ratingInput").val(),
+            }
+            const url = 'https://tender-soft-bayberry.glitch.me/movies/';
+            const options = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(newMovie),
+            };
+            fetch(url, options)
+                .then(response => console.log(response)) /* review was created successfully */
+                .catch(error => console.error(error)); /* handle errors */
     })
+
+
+
     fetch('https://tender-soft-bayberry.glitch.me/movies/')
         .then(response => response.json())
         .then(function (data) {
