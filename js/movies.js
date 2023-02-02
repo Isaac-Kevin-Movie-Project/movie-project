a9b93b909103ae131a5d19797779fabfcff47000
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-02 14:25:58 -0600

    fixed that damn button

--- js/movies.js
+++ js/movies.js
@@ -2,10 +2,14 @@
 
 
     $("#addBtn").click(function () {
-            console.log('hello')
+
+            let newTitle = $("#titleInput").val()
+            let newRating = $("#ratingInput").val()
+
+
             const newMovie = {
-                title: $("#titleInput").val(),
-                rating: $("#ratingInput").val(),
+                title: newTitle,
+                rating: newRating
             }
             const url = 'https://tender-soft-bayberry.glitch.me/movies/';
             const options = {
@@ -22,6 +26,8 @@
 
 
 
+
+
     fetch('https://tender-soft-bayberry.glitch.me/movies/')
         .then(response => response.json())
         .then(function (data) {
@@ -31,14 +37,32 @@
         })
 
 
+    $('btn').click(function (8) {
 
+        function deleteMovie(id) {
+            const url = `https://tender-soft-bayberry.glitch.me/movies/${id}`;
+            const options = {
+                method: 'DELETE',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            };
+            fetch(url, options)
+                .then(response => console.log(response)) /* review was created successfully */
+                .catch(error => console.error(error));
+
+         }
+    })
     function renderMovies(data) {
         let html = ''
         for (let i = 0; i < data.length; i++) {
+            html += '<div class="card">'
             html += '<ul>';
             html += '<li>Title: ' + data[i].title + '</li>';
             html += '<li>Rating: ' + data[i].rating + '</li>';
             html += '</ul>';
+            html += `<a href="#" id="${data[i].id}" class="btn btn-primary">Update</a>\n<a href="#" id="${data[i].id}" class="btn btn-danger">Delete</a>\n`
+            html += '</div>'
         }
 
         return html;
