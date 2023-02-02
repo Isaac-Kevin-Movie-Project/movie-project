3279543dde9eb6afc327967af7e3e6cfbb8c283e
Kevin Fowler <kevin.m.fowler17@gmail.com>
2023-02-02 11:40:53 -0600

    movies project

--- js/movies.js
+++ js/movies.js
@@ -24,6 +24,25 @@
     fetch('https://tender-soft-bayberry.glitch.me/movies/')
         .then(response => response.json())
         .then(function (data) {
+            console.log(data)
+            document.getElementById("movies").innerHTML = renderMovies(data);
 
         })
+
+
+
+    function renderMovies(data) {
+        let html = ''
+        for (let i = 0; i < data.length; i++) {
+            html += '<ul>';
+            html += '<li>Title: ' + data[i].title + '</li>';
+            html += '<li>Rating: ' + data[i].rating + '</li>';
+            html += '</ul>';
+        }
+
+        return html;
+
+    }
+
+
 })();
