599769d3e15b162f5e275bf9a563d8db34f42e4e
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-03 11:36:07 -0600

    added card style

--- js/movies.js
+++ js/movies.js
@@ -130,13 +130,26 @@
      function renderMovies(data) {
         let html = ''
         for (let i = 0; i < data.length; i++) {
-            html += '<div class="card col-3" style="width: 16rem; height: 10rem;">'
-            html += '<ul>';
-            html += '<li>Title: ' + data[i].title + '</li>';
-            html += '<li>Genre: ' + data[i].genre + '</li>';
-            html += '<li>Rating: ' + data[i].rating + '</li>';
-            html += '</ul>';
+            // html += '<div class="card col-3" style="width: 16rem; height: 10rem;">'
+            // html += '<ul>';
+            // html += '<li>Title: ' + data[i].title + '</li>';
+            // html += '<li>Genre: ' + data[i].genre + '</li>';
+            // html += '<li>Rating: ' + data[i].rating + '</li>';
+            // html += '</ul>';
+            // html += `<a href="#" data-id="${data[i].id}" id="dltBtn" class="delete-btn btn btn-danger mx-auto mt-auto m-3">Delete</a>\n`
+            // html += '</div>'
+
+            html += '<div class="container">'
+            html += '<div class="card">'
+            html += '<div class="box">'
+            html += '<div class="content">'
+            html += '<h2>' + data[i].id + '</h2>'
+            html += '<h3>' + data[i].title + '</h3>'
+            html += '<p> Rating: ' + data[i].rating + '</p>'
+            html += '<p> Genre: ' + data[i].genre + '</p>'
             html += `<a href="#" data-id="${data[i].id}" id="dltBtn" class="delete-btn btn btn-danger mx-auto mt-auto m-3">Delete</a>\n`
+            html += '</div>'
+            html += '</div>'
             html += '</div>'
         }
 
