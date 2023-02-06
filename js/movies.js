bef9f6449c93dd043607909dcd803a8fed234c47
Isaac Hernandez <Isaac.a.hernandez12@gmail.com>
2023-02-06 13:04:02 -0600

    changed button color (again)

--- js/movies.js
+++ js/movies.js
@@ -201,7 +201,7 @@
             html += '<div class="container d-flex">'
             if (start === 1) {
                 html += '<div>'
-            };
+            }
             html += '<div class="card">'
             html += '<div class="box">'
             html += '<div class="content">'
@@ -209,14 +209,14 @@
             html += '<h3>' + data[i].title + '</h3>'
             html += '<p> Genre: ' + data[i].genre + '</p>'
             html += '<p> Rating: ' + data[i].rating + '</p>'
-            html += `<a href="#" data-id="${data[i].id}" id="dltBtn" class="delete-btn btn-danger mx-auto mt-auto m-3">Delete</a>\n`
+            html += `<a href="#" data-id="${data[i].id}" id="dltBtn" class="delete-btn btn-dark mx-auto mt-auto m-3">Delete</a>\n`
             html += '</div>'
             html += '</div>'
             html += '</div>'
             if (start === 5) {
                 html += '</div>';
                 start = 0;
-            };
+            }
             html += '</div>'
         }
 
